a159aac521f5374818dc666fbfaf2b7aaf44a073
Nebojsa <lazarevic.nebojsa1005@gmail.com>
2025-04-23 15:26:06 +0200

    initialized app

---
+++ backend/src/index.ts
@@ -0,0 +1,38 @@
+import bodyParser from "body-parser";
+import * as dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+
+dotenv.config();
+
+mongoose.connect(`${process.env.MONGO_DB_URL}`);
+mongoose.connection.once("open", () => {
+  console.log(
+    "\x1b[32m[MongoDB]\x1b[0m" +
+      "\x1b[33m Successfully connected to database \x1b[0m"
+  );
+});
+
+app.use((req, res, next) => {
+  res.setHeader("Content-type", "application/json");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
+app.get("/", (req, res) => {
+  res.send("caos");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(
+    "\x1b[32m[SERVER]\x1b[0m" +
+      "\x1b[33m Server Started On Port \x1b[0m" +
+      `\x1b[32m ${process.env.PORT} \x1b[0m`
+  );
+});
