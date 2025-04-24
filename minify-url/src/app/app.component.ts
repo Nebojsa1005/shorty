4d23c7d334fa66f8491cc9b998d9ddb2e556b873
Nebojsa <lazarevic.nebojsa1005@gmail.com>
2025-04-24 15:03:55 +0200

    added url model

--- minify-url/src/app/app.component.ts
+++ minify-url/src/app/app.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'minify-url';
+  constructor(private http: HttpClient) {}
+
+  async onClick() {
+    // this.http
+    //   .post('http://localhost:3000/api/url/minify', {
+    //     originalUrl: 'http://localhost:3000/api/url/minify',
+    //   })
+    //   .subscribe((e) => console.log(e));
+
+    this.http
+      .get('http://localhost:3000/api/url/get-by-id/68091f1ba3a7e8de7de97b52')
+      .pipe(take(1))
+      .subscribe((e: any) => {
+        window.location.replace(e.originalUrl);
+      });
+  }
 }
