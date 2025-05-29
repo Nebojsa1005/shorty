e4ca8eaac545672fb14ef54e2f29d01aa8292c3b
Nebojsa <lazarevic.nebojsa1005@gmail.com>
2025-05-29 20:26:47 +0200

    ++

--- backend/src/utils/token.ts
+++ backend/src/utils/token.ts
@@ -1,12 +1,4 @@
-import { sign } from "jsonwebtoken";
 
 export const createTokenFromEmailAndId = (email: string, id: string) => {
   return true
-  return sign(
-    {
-      email,
-      id,
-    },
-    `${process.env.JWT_SECRET_KEY}`
-  );
 };
