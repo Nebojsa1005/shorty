0c15bd13f8a4c6a8429b849f871453ca56f5f5b7
Nebojsa <lazarevic.nebojsa1005@gmail.com>
2025-05-29 20:29:30 +0200

    ++

--- shortLink/src/environments/environment.prod.ts
+++ shortLink/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-	apiUrl: 'https://shorty-production-951d.up.railway.app',
+	apiUrl: 'https://shorty-kt3r.onrender.com',
 	oAuthClientId: '1004194683376-tg9e67nqgdmbtjhinc9o2o6hj5lge7b9.apps.googleusercontent.com',
 	oAuthClientSecret: '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'
 }
